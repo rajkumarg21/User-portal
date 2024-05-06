6a3b513805b06cda78dd2f7576cc52ba39b4ba16
Rajkumar saad <58967581+rajkumarg21@users.noreply.github.com>
2024-05-06 12:39:37 +0530

    Add files via upload

---
+++ userportal/src/test/java/com/raj/userportal/UserportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.userportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
